8c4099241ae6b4a87078d45da1942e7a4134025f
Alberto Roman Herrera Espinoza <aherredev@gmail.com>
2019-11-02 05:58:59 -0600

    Completado Pcel scraping

--- server/runtime/runtime_pcel.py
+++ server/runtime/runtime_pcel.py
@@ -5,23 +5,48 @@ path = "https://pcel.com/gamers?sucursal=0&limit=100"
 
 page_response = requests.get(path, timeout=5)
 
+products = []
+
 prices = [] #List of price of all products
 name = [] #List of the name of all products
-print(page_response.content)
+brand=[]
+model = []
+category=[]
+imageDefault = []
 soup = BeautifulSoup(page_response.content,features="html.parser")
-for row in soup.findAll('div',attrs={"class":"product-list"}).findAll('tbody'):
-    cells =  row.find("td")
-    #rating=a.find('div', attrs={'class':'hGSR34 _2beYZw'})
-    info =  cells[0].findAll("a")
-    name.append(info.get("data-name"))
-    page_response = requests.get(info.get("href"))
-    soup2 = BeautifulSoup(page_response.content,features="html.parser")
-    tempPriceNew = soup2.findAll("span",attrs={"class","price-new"})
-    tempPriceOld = soup2.findAll("span",attrs={"class","price-old"})
-    prices.append({"discount":tempPriceNew[0].text,"original":tempPriceOld[0].text})
+tableContext = soup.find_all("div",class_="product-list")
+tableContext = soup.find_all("table")[2]
+rows = tableContext.find_all("tr")
+for row in rows:
+    if(len(row.findAll("div",class_="image"))>0):
+        td = row.find("div",class_="image")
+        cell =  td.find("a")
+        name=cell.get("data-name")
+        imageDefault=cell.find("img").get("src")
+        category=cell.get("data-category").split("/")[1]
+        brand=cell.get("data-brand")
+        page_response2 = requests.get(cell.get("href"))
+        soup2 = BeautifulSoup(page_response2.content,features="html.parser")
+        generalPrice = soup2.findAll("div",attrs={"class","price"})
+        tempPriceNew = soup2.findAll("span",attrs={"class","price-new"})
+        tempPriceOld = soup2.findAll("span",attrs={"class","price-old"})
+        tempModel = soup2.findAll("div",class_="description")
+        tempContent = soup2.findAll("div",class_="description")
+        print(cell.get("data-name"))
+        tempModel = tempModel[0].text.split("\n")
+        tempModel = tempModel[3]
+        tempModel = tempModel.split("Modelo: ")[1]
+        print(tempModel)
+        model=tempModel
+        if(len(tempPriceNew) > 0):
+            prices={"discount":tempPriceNew[0].text.split("$")[1],"original":tempPriceOld[0].text.split("$")[1],"urlImage":imageDefault,"url":cell.get("href"),"site":path}
+        else:
+            prices={"discount":"NoData","original":generalPrice[0].text.split("$")[1],"urlImage":imageDefault,"url":cell.get("href"),"site":path}
+    products.append({"name":name,"price":prices,"content":"","model":model,"marca":brand,"categoria":category,"imageDefault":""})
 
-print(name)
-print("__________________")
-print(products)
+f = open("results.json","w+")
+f.write(str({"data":products}).replace('',""))
+f.close()
+
 #df = pd.DataFrame({'Price':prices}) 
 #df.to_csv('products.csv', index=False, encoding='utf-8')
